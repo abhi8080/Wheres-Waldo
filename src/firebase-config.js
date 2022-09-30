272f51b083449466c75d93796662714206cb4ace
Abhinav Kalra <abhinavkalra2002@gmail.com>
2022-09-30 18:35:29 +0200

    Add Firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCX5KCcAgavPCVjtbiRZdWUtxAp7VEX-wI",
+  authDomain: "where-s-waldo-58777.firebaseapp.com",
+  projectId: "where-s-waldo-58777",
+  storageBucket: "where-s-waldo-58777.appspot.com",
+  messagingSenderId: "350851221586",
+  appId: "1:350851221586:web:4ebb3abe9042b66091f78b"
+};
+
+const app = initializeApp(firebaseConfig);
