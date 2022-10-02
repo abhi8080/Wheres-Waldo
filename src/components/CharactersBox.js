cc5b26d5b94cad2155a28b8331e0ff42f2942460
abhi8080 <abhinavkalra2002@gmail.com>
2022-10-02 07:20:18 +0000

    Add character box

---
+++ src/components/CharactersBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default class CharactersBox extends React.Component {
+    render() {
+        return(
+            <div className="characters-picture" id="characters-picture">
+                <button className="close-button">&times;</button>
+            </div>
+        )    
+    }
+}
