38abd4c1c0e99b7a2f6ed0ce3f8b38a534130ebd
abhi8080 <abhinavkalra2002@gmail.com>
2022-10-04 13:32:34 +0000

    Add a timer and provide feedback after clicking

--- src/components/Photograph.js
+++ src/components/Photograph.js
@@ -2,37 +2,67 @@ import React from "react";
 
 import "../styles/Photograph.css";
 
+import GameOver from "./GameOver";
+
 export default class PhotoGraph extends React.Component {
   constructor(props) {
     super(props);
-    this.createCharactersBox();
-    console.log("here");
+    this.timer = null;
+    this.state = { minutes: 0, seconds: 0 };
+    if (props.example) {
+      console.log("here");
+      clearInterval(this.timer);
+    }
   }
-  createCharactersBox() {
-    const picture = document.createElement("div");
-    picture.classList.add("characters-picture");
-    picture.id = "characters-picture";
-    console.log(picture);
-    document.getElementById("root").appendChild(picture);
+  componentDidMount() {
+    this.timer = setInterval(this.tick, 1000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick = () => {
+    this.setState({ seconds: this.state.seconds + 1 });
+    if (this.state.seconds == 60) {
+      this.setState({ seconds: 0 });
+      this.setState({ minutes: this.state.minutes + 1 });
+    }
+    console.log(this.state.seconds);
+  };
   render() {
     return (
       <div id="photograph-section">
         <nav>
-          <button id="view-characters">View all characters</button>
-          <div id="timer">Timer</div>
+          <div id="incorrect-message">Incorrect, try again</div>
+          <button id="view-characters">
+            View all characters {this.props.example}
+          </button>
+          <button id="timer" onClick={() => console.log("It works")}>
+            Timer
+          </button>
         </nav>
         <div id="photo">
           <div id="target-box">.</div>
           <div id="dropdown-select">
-            <span>Waldo</span>
-            <span>Woof the dog</span>
-            <span>Wilma</span>
-            <span>Wizard Whitebeard</span>
-            <span>Odlaw</span>
+            <span className="character" id="waldo">
+              Waldo
+            </span>
+            <span className="character" id="woof">
+              Woof the dog
+            </span>
+            <span className="character" id="wilma">
+              Wilma
+            </span>
+            <span className="character" id="wizard">
+              Wizard Whitebeard
+            </span>
+            <span className="character" id="odlaw">
+              Odlaw
+            </span>
           </div>
         </div>
+        <GameOver seconds={this.state.seconds} />
       </div>
     );
   }
