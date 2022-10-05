fe1db7a2c6fc4e2566979edffdf85a21c283c00f
Abhinav Kalra <abhinavkalra2002@gmail.com>
2022-10-05 12:05:52 +0200

    Add function getPlayers

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,11 @@
-import { initializeApp } from 'firebase/app';
-import {getFirestore , doc, collection, getDoc} from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  doc,
+  collection,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -8,17 +14,30 @@ const firebaseConfig = {
   projectId: "where-s-waldo-58777",
   storageBucket: "where-s-waldo-58777.appspot.com",
   messagingSenderId: "350851221586",
-  appId: "1:350851221586:web:4ebb3abe9042b66091f78b"
+  appId: "1:350851221586:web:4ebb3abe9042b66091f78b",
 };
 initializeApp(firebaseConfig);
 const db = getFirestore();
 
 export default async function getLocation(character) {
-  const docRef = doc(db, 'characters', `${character}`);
+  const docRef = doc(db, "characters", `${character}`);
   const docSnap = await getDoc(docRef);
 
   const xValue = docSnap.data().xValue;
   const yValue = docSnap.data().yValue;
-  return {xValue, yValue}
+  return { xValue, yValue };
+}
 
+export async function getPlayers() {
+  let leaderboard = [];
+  const colRef = collection(db, "playersOnLeaderboard");
+  const snapshot = await getDocs(colRef);
+  snapshot.docs.forEach((doc) => {
+    const player = {
+      name: doc.data().Name,
+      score: doc.data().Score,
+    };
+    leaderboard.push(player);
+  });
+  return leaderboard;
 }
