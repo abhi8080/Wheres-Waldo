b2151d488a1d86302a4cd07ca2bde15579c403c1
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-01-07 22:04:57 +0100

    Add CharactersBox and Overlay

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CharactersBox from "./components/CharactersBox";
+import Overlay from "./components/Overlay";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+    <CharactersBox />
+    <Overlay />
+  </>
 );
